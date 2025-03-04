68e9f3e8370fbd726d0f2278fabf0d54b060e577
Niraj Kumar <nirajgupta7481@gmail.com>
2025-03-05 00:26:39 +0530

    [Niraj] Add . added name validation using regex with try-catch error handling

--- employeewage.js
+++ employeewage.js
@@ -315,3 +315,49 @@ let employeePayrollData12 = new EmployeePayrollData12(1, "Mark", 30000, "M", new
 console.log(employeePayrollData12.toString());
 let newEmployeePayrollData = new EmployeePayrollData12(1, "Terrisa", 30000, "F", new Date());
 console.log(newEmployeePayrollData.toString());
+
+// UC13 - Use Regex to validate name and throw error if name is incorrect
+class EmployeePayrollData13 {
+    id;
+    salary;
+    gender;
+    startDate;
+
+    // Constructor
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
+    }
+
+    // Getter and setter
+    get name() { return this._name; }
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (nameRegex.test(name)) this._name = name;
+        else throw "Name is incorrect!";
+    }
+
+    // Method
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = this.startDate === undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id=" + this.id + ", name=" + this.name + ", salary=" +
+            this.salary + ","+
+            "gender=" + this.gender + ", startDate=" + empDate;
+    }
+}
+
+let employeePayrollData13 = new EmployeePayrollData13(1, "Mark", 30000, "M", new Date());
+console.log(employeePayrollData13.toString());
+try {
+    employeePayrollData13.name = "john";
+    console.log(employeePayrollData13.toString());
+}
+catch (e) {
+    console.error(e);
+}
+let newEmployeePayrollData13 = new EmployeePayrollData13(1, "Terrisa", 30000, "F", new Date());
+console.log(newEmployeePayrollData13.toString());
